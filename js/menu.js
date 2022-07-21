e1f0046042f51ce1362cc8c8c49799168f48a4f9
arqu33n <soondawn@yandex.ru>
2022-07-21 19:31:49 +0300

    2day

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,8 @@ const headerNavigation = document.querySelector(".header__navigation");
 headerNavBtn.addEventListener("click", () => {
   headerNavigation.classList.toggle("header__navigation_open");
 });
+const demoLessonBtn = document.querySelector(".start_demo_lesson_next_button");
+demoLessonBtn.addEventListener("click", () => {
+  self.sdk.track("banner", getEventProperties("click", true));
+  console.log("Переход к Демо-уроку");
+});
